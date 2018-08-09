477eb7e6b921874dffcab886d4ff9661726c08a0
Sergej Sintschilin <seregpie@gmail.com>
2018-08-09 12:01:49 +0200

    +

--- src/getOptimalFontSize.js
+++ src/getOptimalFontSize.js
@@ -1,8 +1,8 @@
 import {Vector3} from 'three';
 
-const objectWorldPosition = new Vector3();
-const cameraWorldPosition = new Vector3();
-const objectWorldScale = new Vector3();
+let objectWorldPosition = new Vector3();
+let cameraWorldPosition = new Vector3();
+let objectWorldScale = new Vector3();
 
 export default function(object, renderer, camera) {
 	if (renderer.domElement.width && renderer.domElement.height && object.material.map.textLines.length) {
