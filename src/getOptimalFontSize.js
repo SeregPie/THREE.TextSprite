13b38778b13e5ed334d8eda7d4799e2d50d307d3
Erik Buchholz <erik.buchholz@arkadium.com>
2019-01-29 15:44:00 -0500

    Font size calculation accounts for camera fov, so text is no longer blurry at low fov's

--- src/getOptimalFontSize.js
+++ src/getOptimalFontSize.js
@@ -8,7 +8,11 @@ export default function(object, renderer, camera) {
 	if (renderer.domElement.width && renderer.domElement.height && object.material.map.textLines.length) {
 		let distance = object.getWorldPosition(objectWorldPosition).distanceTo(camera.getWorldPosition(cameraWorldPosition));
 		if (distance) {
-			let heightInPixels = object.getWorldScale(objectWorldScale).y * renderer.domElement.height / distance;
+                        // Get the height of the view in world units at the depth of the object
+                        let vFOV = camera.fov * Math.PI / 180; 
+                        let fullHeight = 2 * Math.tan( vFOV / 2 ) * Math.abs( distance );
+
+                        let heightInPixels = (object.getWorldScale(objectWorldScale).y / fullHeight) * renderer.domElement.height;
 			if (heightInPixels) {
 				return Math.round(heightInPixels / object.material.map.imageHeight);
 			}
