2112137f1ea80d46a5dd6297417ea2c220babea2
Sergej Sintschilin <seregpie@gmail.com>
2020-10-12 16:17:43 +0200

    add types declaration file

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+import {Sprite} from 'three';
+
+export default class TextSprite extends Sprite {
+	constructor(options?: {
+		alignment?: string;
+		color?: string;
+		fontFamily?: string;
+		fontSize?: number;
+		fontStyle?: string;
+		fontVariant?: string;
+		fontWeight?: string;
+		lineGap?: number;
+		padding?: number;
+		strokeColor?: string;
+		strokeWidth?: number;
+		text?: string;
+	});
+
+	readonly isTextSprite: true;
+
+	text: string;
+
+	fontFamily: string;
+
+	fontSize: number;
+
+	fontWeight: string;
+
+	fontVariant: string;
+
+	fontStyle: string;
+
+	color: string;
+
+	strokeWidth: number;
+
+	strokeColor: string;
+
+	alignment: string;
+
+	lineGap: number;
+
+	padding: number;
+
+	dispose(): void;
+};
